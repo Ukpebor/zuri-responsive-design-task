1e5fd719051fdb3eca6dba7fbb6fc193c2732573
Ukpebor Nicholas Osakpolor <65134147+Ukpebor@users.noreply.github.com>
2021-04-11 10:54:03 +0100

    Update burger.js

--- burger.js
+++ burger.js
@@ -1,24 +1,10 @@
-const navSlide = ()=> {
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.nav-links');
-    const navLinks = document.querySelectorAll('.nav-links li')
+const burger = document.querySelector(".burger");
+const navLinks = document.querySelector(".nav-links");
+const links = document.querySelectorAll(".nav-links li");
 
-
-
-
-    burger.addEventListener('click', () => {
-        //toggle
-        nav.classList.toggle('nav-active');
-
-        //animate links
-        navLinks.forEach((link, index) =>{
-            if(link.style.animation) {
-                link.style.animation = '';
-            } else {
-                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 * 0.3}s`
-            }
-        })
-    })
-}
-
-navSlide();
+burger.addEventListener("click", () => {
+  navLinks.classList.toggle("open");
+  links.forEach(link => {
+    link.classList.toggle("fade");
+  });
+});
