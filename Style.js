04ac2fd77b5d2de19c31dedfae2c1a5b6c4e89c1
Ukpebor Nicholas Osakpolor <65134147+Ukpebor@users.noreply.github.com>
2021-04-11 12:41:54 +0100

    Update Style.js

--- Style.js
+++ Style.js
@@ -1,10 +1,11 @@
-const burger = document.querySelector(".burger");
-const navLinks = document.querySelector(".nav-links");
+
+const hamburger = document.querySelector(".hamburger");
+const navLinks  = document.querySelector(".nav-links");
 const links = document.querySelectorAll(".nav-links li");
 
-burger.addEventListener("click", () => {
-  navLinks.classList.toggle("open");
-  links.forEach(link => {
-    link.classList.toggle("fade");
-  });
+hamburger.addEventListener("click", () => {
+    navLinks.classList.toggle("open");
+    links.forEach(link =>{
+        link.classList.toggle("fade");
+    });
 });
